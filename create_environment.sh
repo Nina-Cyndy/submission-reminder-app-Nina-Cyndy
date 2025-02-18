b92224899a13f1c638fd6a509cacaf9a107d8e5b
Nina-Cyndy <n.bwiza@alustudent.com>
2025-02-18 17:52:24 +0200

    Populated the files

--- create_environment.sh
+++ create_environment.sh
@@ -1,7 +1,25 @@
-
 #!/bin/bash
-echo "Welcome! Enter your name please: "
+
+# Step 1: Prompt for the user's name
+echo "Enter your name:"
 read user_name
-main_dir="submission_reminder_$user_name"
-mkdir -p "$main_dir"
-echo "Environment Setup Complete!"
+
+# Step 2: Create the main directory
+mkdir "submission_reminder_${user_name}"
+
+# Step 3: Go into the main directory to start creating subdirectories
+cd "submission_reminder_${user_name}"
+
+# Step 4: Create subdirectories inside the main directory
+mkdir app
+mkdir assets
+mkdir config
+mkdir modules
+
+# Step 5: Create necessary files in the respective directories
+touch app/reminder.sh
+touch assets/submissions.txt
+touch config/config.env
+touch modules/functions.sh
+
+echo "Environment setup completed!"
